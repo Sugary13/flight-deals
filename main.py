0f15859c605c5eaa07ad19f6c9978c24771d31e7
Sugary13 <146895409+Sugary13@users.noreply.github.com>
2025-09-04 00:39:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import time
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+# print(sheet_data)
+flight_search = FlightSearch()
+
+
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
