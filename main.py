fe351bab73a31242281dd9ac574ea95a8f2dcfde
Sugary13 <146895409+Sugary13@users.noreply.github.com>
2025-09-05 22:03:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import time
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
 flight_search = FlightSearch()
-
 
 
 for row in sheet_data:
@@ -19,3 +17,17 @@ print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+
+
+destinations = data_manager.get_destination_data()
+
+for city in destinations:
+    iata_code = city["iataCode"]
+
+    flight_offer = flight_search.find_cheapest_price(iata_code)
+
+    if flight_offer:
+        flight_data = FlightData()
+        formatted = flight_data.flight_data(flight_offer)
+        print(formatted)
